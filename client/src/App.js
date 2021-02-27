37488023d8cd41142a46ca3d462f49dc3c01cab7
Joseph bayad <jobayad66@gmail.com>
2021-02-27 01:31:25 +0100

    jsx

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,84 @@ import 'app.css';
 function App() {
   return (
     <div className='App'>
-      <h1>Hello Mern stack</h1>
+      <div className='information'>
+        <label>Name:</label>
+        <input
+          type='text'
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+        />
+        <label>Age:</label>
+        <input
+          type='number'
+          onChange={(event) => {
+            setAge(event.target.value);
+          }}
+        />
+        <label>Country:</label>
+        <input
+          type='text'
+          onChange={(event) => {
+            setCountry(event.target.value);
+          }}
+        />
+        <label>Position:</label>
+        <input
+          type='text'
+          onChange={(event) => {
+            setPosition(event.target.value);
+          }}
+        />
+        <label>Wage (year):</label>
+        <input
+          type='number'
+          onChange={(event) => {
+            setWage(event.target.value);
+          }}
+        />
+        <button onClick={addEmployee}>Add Employee</button>
+      </div>
+      <div className='employees'>
+        <button onClick={getEmployees}>Show Employees</button>
+
+        {employeeList.map((val, key) => {
+          return (
+            <div className='employee'>
+              <div>
+                <h3>Name: {val.name}</h3>
+                <h3>Age: {val.age}</h3>
+                <h3>Country: {val.country}</h3>
+                <h3>Position: {val.position}</h3>
+                <h3>Wage: {val.wage}</h3>
+              </div>
+              <div>
+                <input
+                  type='text'
+                  placeholder='2000...'
+                  onChange={(event) => {
+                    // setNewWage(event.target.value);
+                  }}
+                />
+                <button
+                  onClick={() => {
+                    // updateEmployeeWage(val.id);
+                  }}>
+                  {' '}
+                  Update
+                </button>
+
+                <button
+                  onClick={() => {
+                    // deleteEmployee(val.id);
+                  }}>
+                  Delete
+                </button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -8,6 +8,24 @@ const db = mysql.createConnection({
   host: 'localhost',
   database: 'employSystem',
 });
+
+app.post('/create', (req, res) => {
+  const name = req.body.name;
+  const position = req.body.position;
+  const age = req.body.age;
+  const country = req.body.country;
+  const wage = req.body.wage;
+
+  db.query(
+    'INSERT INTO employees (name,age,position,country,wage) VALUES (?,?,?,?,?)',
+    [name, age, position, country, wage],
+    (e, r) => {
+      if (e) console.log(e);
+      else res.send('values inserted');
+    }
+  );
+});
+
 app.listen(3001, () => {
   console.log('your server is running');
 });
