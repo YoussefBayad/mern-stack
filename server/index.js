8832cddfd779c89db7526fe8228844568181e375
Joseph bayad <jobayad66@gmail.com>
2021-02-27 01:15:47 +0100

    seting up db

--- server/index.js
+++ server/index.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
+const mysql = require('mysql');
 
+const db = mysql.createConnection({
+  user: 'root',
+  password: '',
+  host: 'localhost',
+  database: 'employSystem',
+});
 app.listen(3001, () => {
   console.log('your server is running');
 });
